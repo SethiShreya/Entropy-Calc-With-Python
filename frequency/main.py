c0ec924af8b5c9b43746962c1af715a3f7162529
shreya <shreyasethi47@gmail.com>
2022-03-30 11:47:23 +0530

    done some changes

--- frequency/main.py
+++ frequency/main.py
@@ -2,7 +2,13 @@ import csv
 from math import log2
 import os
 from collections import Counter as count
-# os.chdir(".\\frequency")
+# check if the file exist in current directory
+file_exists = os.path.exists('input.csv')
+
+if (file_exists):
+    pass
+else:
+     os.chdir("./frequency")
 print(os.getcwd())
 
 def shannon(freq, size):
@@ -20,8 +26,11 @@ def frequency(listob):
     with open("shannon.txt", 'w') as shan:
         with open("tsallis.txt", 'w') as tsal:
             for i in freq:
-                shan.write(str(shannon(freq[i], size))+'\n')
-                tsal.write(str(tsallis(freq[i], size, q))+'\n')
+                shan.write(f"Shannon entropy of {i} is {str(shannon(freq[i], size))}\n")
+                tsal.write(f"Tsallis entropy of {i} is {str(tsallis(freq[i], size, q))}\n")
+                # shan.write(str(shannon(freq[i], size))+'\n')
+                # tsal.write(str(tsallis(freq[i], size, q))+'\n')
+
 
 inplist = []
 with open("input.csv") as f:
