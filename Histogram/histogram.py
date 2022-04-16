9036e6ff5eb29e82fa2a887ff494ab47f73bc3ec
shreya <shreyasethi47@gmail.com>
2022-04-16 16:46:28 +0530

    added some changes to the histogram codes

--- Histogram/histogram.py
+++ Histogram/histogram.py
@@ -1,8 +1,17 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 
+# reading csv file from the path
 df= pd.read_csv("./Histogram/shannon_Out.csv")
 
+# plotting histogram
 plt.hist(df)
+
+# Giving label to the graph
+plt.title("Entropy Data")
+plt.xlabel("Entropy")
+plt.ylabel("Frequency")
+
+# saving histogram plt to the specifies location
 plt.savefig('./Histogram/histogram.png')
 # plt.show()
