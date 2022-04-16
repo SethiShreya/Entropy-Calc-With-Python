b43f833af9b948ad9b41ea95444876377d582daa
shreya <shreyasethi47@gmail.com>
2022-04-16 17:17:49 +0530

    make some more graphs

--- Histogram/histogram.py
+++ Histogram/histogram.py
@@ -1,17 +1,29 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 
-# reading csv file from the path
-df= pd.read_csv("./Histogram/shannon_Out.csv")
+def makeHistogram(df, title, xlabel, ylabel, outputFileName):
+    # reading csv file from the path
+    
 
-# plotting histogram
-plt.hist(df)
+    # plotting histogram
+    plt.hist(df)
 
-# Giving label to the graph
-plt.title("Entropy Data")
-plt.xlabel("Entropy")
-plt.ylabel("Frequency")
+    # Giving label to the graph
+    plt.title("Entropy Data")
+    plt.xlabel("Entropy")
+    plt.ylabel("Frequency")
 
-# saving histogram plt to the specifies location
-plt.savefig('./Histogram/histogram.png')
-# plt.show()
+    # saving histogram plt to the specifies location
+    plt.savefig(f'./Histogram/{outputFileName}.png')
+    # plt.show()
+
+df= pd.read_csv("./frequency/shannon.csv")
+df2= pd.read_csv("./frequency/tsallis for q = 2.csv")
+df3= pd.read_csv("./frequency/tsallis for q = 3.csv")
+df4= pd.read_csv("./frequency/tsallis for q = 4.csv")
+
+makeHistogram(df, "Shannon Entropy Histogram", "Frequency", "Shannon Entropy", "Shannon")
+makeHistogram(df2, "Tsallis Entropy Histogram", "Frequency", "Tsallis Entropy for q = 2", "Tsallis for q=2")
+makeHistogram(df3, "Tsallis Entropy Histogram", "Frequency", "Tsallis Entropy for q = 3", "Tsallis for q=3")
+makeHistogram(df4, "Tsallis Entropy Histogram", "Frequency", "Tsallis Entropy for q = 4", "Tsallis for q=4")
+
